937abd0a0fe11b0a2ce52cb26f9a2142b92b2e73
josh Lerner <69250030+Josh-lerner@users.noreply.github.com>
2020-10-26 19:02:36 -0400

    Create head.js

---
+++ head.js
@@ -0,0 +1,20 @@
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍👍👍 Assertion Passed:  ${actual}   ===   ${expected}`);
+  }
+  if (actual !== expected) {
+    console.log(`👎👎👎 Assertion Failed:  ${actual}   !==   ${expected}`);
+  }
+};
+
+const head = function(array){
+  for (let i = 0; i < array.length; i++) {
+    return array[0];
+  }
+  
+
+
+}
+assertEqual(head([5,6,7]),5)
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
