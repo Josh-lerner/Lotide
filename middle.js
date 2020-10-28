0e31ce99b65bbcfedb65226e91aba033c92e9dfb
josh Lerner <69250030+Josh-lerner@users.noreply.github.com>
2020-10-28 14:00:24 -0400

    Create middle.js

---
+++ middle.js
@@ -0,0 +1,23 @@
+ const middle = function (array){
+   let num = 0
+   // iterate over the function 
+    for ( i = 0; i < array.length; i++){
+      // condition if index is 2 or less return empty array
+      if (array.length <= 2){
+        return num = []
+      }
+      // if length is odd get the middle number
+      else if ( array.length % 2 !== 0){
+        num = [Math.ceil(array.length /2)] 
+  
+      }
+          // if length is even get middle 2 numbers
+    else if ( array.length % 2 === 0){
+      num = [array.length /2, array.length/2 +1]
+    
+    }
+  } 
+    return num
+ 
+  }  
+  console.log(middle([1,2,3,4,5,6,]))
