69372d0d71bd11638546cd1afe1315b6f06e0f09
josh Lerner <69250030+Josh-lerner@users.noreply.github.com>
2020-10-31 20:15:38 -0400

    Create without.js
    needs work

---
+++ without.js
@@ -0,0 +1,16 @@
+const without = function(words, remove){
+  let newArr = [];
+  let wordToTakeOut = remove;
+  for (let i = 0; i < words.length; i++){
+    if (words[i] === wordToTakeOut){
+     console.log(words.splice(0, 2))
+   
+    } 
+  
+    
+   
+ 
+  
+}
+}
+ without(['hello', 'world', 'lighthouse'], 'lighthouse')
