3744c120c271aee8da58da6dcf6ffab69a848bd6
josh Lerner <69250030+Josh-lerner@users.noreply.github.com>
2020-11-02 13:16:17 -0500

    Update and rename eqArray.js to eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,12 @@
+const assertEqual = require('./assertEqual')
+
+
+
+const eqArrays = function(array1, array2){
+for (var i=0; i<array1.length; i++) 
+   if(array1[i]!= array2[i])  
+  return false; 
+    return true; 
+
+}
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true))
