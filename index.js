d02d1292a3ce29a86ee6bdb003620904ad523f44
josh Lerner <69250030+Josh-lerner@users.noreply.github.com>
2020-11-02 17:34:02 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const countLetters = require('./countLetters');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const without = require('./without');
+const takeUntil = require('./takeUntil');
+const eqarrays = require('./eqarrays');
+const eqObjects = require('./eqObjects');
+
+module.exports = {
+  head,
+  tail,   
+  middle,
+  countLetters,
+  findKey,
+  findKeyByValue,
+  letterPositions,
+  map,
+  without,
+  takeUntil,
+  eqarrays,
+  eqObjects,
+};
+
+// >  const _ = require('./index')
+// >  _
