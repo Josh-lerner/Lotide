85bb4b3a31d7184159fe53869004e9820707b841
josh Lerner <69250030+Josh-lerner@users.noreply.github.com>
2020-11-02 16:26:17 -0500

    Create findKeyTest.js

---
+++ findKeyTest.js
@@ -0,0 +1,21 @@
+const assert = require('chai').assert;
+const findKey   = require('../findKey');
+
+describe("#findKey", () => {
+  it(`returns noma for {
+    "Blue Hill": { stars: 1 },
+    "Akaleri": { stars: 3 },
+    "noma": { stars: 2 },
+    "elBulli": { stars: 3 },
+    "Ora": { stars: 2 },
+    "Akelarre": { stars: 3 },`, () => {
+    assert.deepEqual(findKey({
+      "Blue Hill": { stars: 1 },
+      "Akaleri": { stars: 3 },
+      "noma": { stars: 2 },
+      "elBulli": { stars: 3 },
+      "Ora": { stars: 2 },
+      "Akelarre": { stars: 3 },
+    }, planet => planet.stars === 2), "noma");
+  });
+});
