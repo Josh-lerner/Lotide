6b645bb1e2a140b617225f636befe07a509402fc
josh Lerner <69250030+Josh-lerner@users.noreply.github.com>
2020-11-02 13:50:02 -0500

    Update head.js

--- head.js
+++ head.js
@@ -4,9 +4,6 @@ const head = function(array){
   for (let i = 0; i < array.length; i++) {
     return array[0];
   }
-  
-
-
 }
-assertEqual(head([5,6,7]),5)
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+module.exports = head;
