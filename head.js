49de8ed4fba9d42508d6e7a80a7aa08e0e7ce3ca
josh Lerner <69250030+Josh-lerner@users.noreply.github.com>
2020-11-02 13:10:50 -0500

    Update head.js

--- head.js
+++ head.js
@@ -1,20 +1,12 @@
+const assertEqual = require('./assertEqual')
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍👍👍 Assertion Passed:  ${actual}   ===   ${expected}`);
-  }
-  if (actual !== expected) {
-    console.log(`👎👎👎 Assertion Failed:  ${actual}   !==   ${expected}`);
-  }
-};
-
-const head = function(array) {
+const head = function(array){
   for (let i = 0; i < array.length; i++) {
     return array[0];
   }
   
 
 
-};
-assertEqual(head([5,6,7]),5);
+}
+assertEqual(head([5,6,7]),5)
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
