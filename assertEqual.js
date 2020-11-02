7750bdabcd2f571a219f4facc70c4705708c8aae
josh Lerner <69250030+Josh-lerner@users.noreply.github.com>
2020-11-02 13:11:24 -0500

    Update assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,12 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("👍👍👍 Assertion Passed: " + actual + " === " + expected);
+    console.log(`👍👍👍 Assertion Passed:  ${actual}   ===   ${expected}`);
   }
   if (actual !== expected) {
-    console.log("👎👎👎 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`👎👎👎 Assertion Failed:  ${actual}   !==   ${expected}`);
   }
 };
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual(2, 1);
+// assertEqual("Lighthouse Labs", "Lighthouse Labs");
+// assertEqual(2, 1);
+
+module.exports = assertEqual;
