eb01a60adfcfa5a8d7437acbfd13a4f137c00626
josh Lerner <69250030+Josh-lerner@users.noreply.github.com>
2020-11-02 13:12:32 -0500

    Update tail.js

--- tail.js
+++ tail.js
@@ -1,12 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍👍👍 Assertion Passed:  ${actual}   ===   ${expected}`);
-  }
-  if (actual !== expected) {
-    console.log(`👎👎👎 Assertion Failed:  ${actual}   !==   ${expected}`);
-  }
-};
-
+const assertEqual = require('./assertEqual')
 
 const words = ["Yo Yo", "Lighthouse", "Labs",];
 const tail = function(array) {
