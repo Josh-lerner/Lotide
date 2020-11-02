6ac5a5605d53838796ce3997671d8caabae2a894
josh Lerner <69250030+Josh-lerner@users.noreply.github.com>
2020-11-02 14:06:41 -0500

    Update tail.js

--- tail.js
+++ tail.js
@@ -1,10 +1,9 @@
-const assertEqual = require('./assertEqual')
 
-const words = ["Yo Yo", "Lighthouse", "Labs",];
+
 const tail = function(array) {
   for (let i = 0; i < array.length; i++) {
     return array.slice(1);
   }
 };
-tail(words);
-assertEqual(words.length, 3);
+
+module.exports = tail;
